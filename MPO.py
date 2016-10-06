dc093bce9e32117d9d0b361908417a84737cabe3
rezah <rezahaghshenass@gmail.com>
2016-10-06 21:31:56 +0330

    Add mpo functions: mpo_list(initialize) , mpo_U_list

--- MPO.py
+++ MPO.py
@@ -1,6 +1,6 @@
 import pyUni10 as uni10
 #import sys
-import numpy as np
+#import numpy as np
 #import matplotlib.pyplot as plt
 #import matplotlib
 #import pylab
@@ -18,6 +18,20 @@ def matSy():
 
 def matSz():
   return uni10.Matrix(2, 2, [0.50, 0.0, 0.0, -0.50])
+
+
+
+def intialize_coupling(L, hz_list, W): 
+ for i in xrange(L):
+  hz_list.append(random.uniform(-W,W))
+
+
+def print_trH2(L, hz_list,J): 
+ hz_list_2=[x**2 for x in hz_list]
+ trH2=(2.00**L)*( J**2 * 3.0/16.00 * (L-1) + 1.0/4.0 * sum(hz_list_2))
+ #print trH2
+
+
 
 
 def make_mpo_H(L, J, hz_list ):
@@ -72,7 +86,11 @@ def make_mpo_H(L, J, hz_list ):
  mpo_uni10_br_2.setName('mpo_uni10_br_2')
 ##############################################
 
-
+ mpo_uni10_list_boundry=[]
+ mpo_uni10_list_boundry.append(mpo_uni10_bl)
+ mpo_uni10_list_boundry.append(mpo_uni10_br)
+ mpo_uni10_list_boundry.append(mpo_uni10_bl_2)
+ mpo_uni10_list_boundry.append(mpo_uni10_br_2)
 
 #################### Define MPO ########################
  mpo_matrix=uni10.Matrix(chi*d,chi*d)
@@ -101,11 +119,11 @@ def make_mpo_H(L, J, hz_list ):
         if(mu==4 and mup==0  ):
            mpo_matrix[mu*d*d*chi+s*d*chi+mup*d+sp]=float(hz_list[num])*sz[s*d+sp]
         if(mu==4 and mup==1  ):
-           mpo_matrix[mu*d*d*chi+s*d*chi+mup*d+sp]=sx[s*d+sp]
+           mpo_matrix[mu*d*d*chi+s*d*chi+mup*d+sp]=float(J)*sx[s*d+sp]
         if(mu==4 and mup==2  ):
-           mpo_matrix[mu*d*d*chi+s*d*chi+mup*d+sp]=-sy[s*d+sp]
+           mpo_matrix[mu*d*d*chi+s*d*chi+mup*d+sp]=-float(J)*sy[s*d+sp]
         if(mu==4 and mup==3  ):
-           mpo_matrix[mu*d*d*chi+s*d*chi+mup*d+sp]=sz[s*d+sp]
+           mpo_matrix[mu*d*d*chi+s*d*chi+mup*d+sp]=float(J)*sz[s*d+sp]
         if(mu==4 and mup==4  ):
            mpo_matrix[mu*d*d*chi+s*d*chi+mup*d+sp]=s0[s*d+sp]
 
@@ -127,22 +145,19 @@ def make_mpo_H(L, J, hz_list ):
    mpo_uni10_cotraction.setName('mpo_uni10_4')
    mpo_uni10_4_list.append(mpo_uni10_cotraction)
    
- #print mpo_uni10_1_list[1].printDiagram()
- return mpo_uni10_1_list, mpo_uni10_4_list, mpo_uni10_bl , mpo_uni10_br, mpo_uni10_bl_2, mpo_uni10_br_2 
+ ##printmpo_uni10_1_list[1].printDiagram()
+ 
+ return mpo_uni10_1_list, mpo_uni10_4_list, mpo_uni10_list_boundry
 
 
 
-
-
-
-def contraction_MPO_trH2(mpo_uni10_1_list, mpo_uni10_4_list,  mpo_uni10_bl, mpo_uni10_br, mpo_uni10_bl_2, mpo_uni10_br_2):
+def contraction_MPO_trH2(mpo_uni10_1_list, mpo_uni10_4_list,  mpo_boundy_list):
  mpo_uni10_2_list=[]
  mpo_uni10_3_list=[]
  mpo_uni10_5_list=[]
  mpo_uni10_6_list=[]
- 
+ mpo_uni10_1_listN=[copy.copy(mpo_uni10_1_list[i])  for i in xrange(len(mpo_uni10_1_list)) ] 
  mpo_uni10_4_listN=[copy.copy(mpo_uni10_4_list[i]) for i in xrange(len(mpo_uni10_4_list)) ]
- mpo_uni10_1_listN=[copy.copy(mpo_uni10_1_list[i])  for i in xrange(len(mpo_uni10_1_list)) ]
  L=len(mpo_uni10_1_listN)
  for num in xrange(L):
   mpo_uni10_prime=copy.copy(mpo_uni10_1_listN[num])
@@ -164,9 +179,9 @@ def contraction_MPO_trH2(mpo_uni10_1_list, mpo_uni10_4_list,  mpo_uni10_bl, mpo_
   mpo_uni10_2_prime.setName("mpo_uni10_3")
   mpo_uni10_3_list.append(mpo_uni10_2_prime)
 
- #print mpo_uni10_list[0], mpo_uni10_list[1]
- #print mpo_uni10_2_list[0], mpo_uni10_2_list[1]
- #print mpo_uni10_3_list[0], mpo_uni10_3_list[1]
+ ##printmpo_uni10_list[0], mpo_uni10_list[1]
+ ##printmpo_uni10_2_list[0], mpo_uni10_2_list[1]
+ ##printmpo_uni10_3_list[0], mpo_uni10_3_list[1]
 
  mpo_uni10_3_list_prime=[copy.copy(mpo_uni10_3_list[i])  for i in xrange(len(mpo_uni10_3_list)) ]
 
@@ -190,9 +205,10 @@ def contraction_MPO_trH2(mpo_uni10_1_list, mpo_uni10_4_list,  mpo_uni10_bl, mpo_
     
 
  #############  Testing calcualtion of trace H^{2} ###########################
+ mpo_uni10_br_2=copy.copy(mpo_boundy_list[3])
  mpo_uni10_br_2.setLabel([1])
- trH2=mpo_uni10_bl_2*mpo_uni10_3_list_prime[0]*mpo_uni10_br_2
- print trH2[0]
+ trH2=mpo_boundy_list[2]*mpo_uni10_3_list_prime[0]*mpo_uni10_br_2
+ #printtrH2[0]
  ###########################################################################
 
 
@@ -220,7 +236,7 @@ def contraction_MPO_trH2(mpo_uni10_1_list, mpo_uni10_4_list,  mpo_uni10_bl, mpo_
   mpo_uni10_cotraction3.setLabel([0,1])
   mpo_uni10_cotraction3.setName('mpo_uni10_6')
   mpo_uni10_6_list.append(mpo_uni10_cotraction3)
-  #print mpo_uni10_5_list[0], mpo_uni10_6_list[0] 
+  ##printmpo_uni10_5_list[0], mpo_uni10_6_list[0] 
   
 
 
@@ -244,15 +260,372 @@ def contraction_MPO_trH2(mpo_uni10_1_list, mpo_uni10_4_list,  mpo_uni10_bl, mpo_
     mpo_uni10_ctraction.setName('mpo_uni10_ctraction')
     mpo_uni10_6_list_prime[0]=mpo_uni10_ctraction
 
- #print mpo_uni10_6_list_prime[0]
+ ##printmpo_uni10_6_list_prime[0]
 
  ##############  Testing calcualtion of trace H^{2} ###########################
+ mpo_uni10_br_2=copy.copy(mpo_boundy_list[3])
  mpo_uni10_br_2.setLabel([1])
- trH2=mpo_uni10_bl_2*mpo_uni10_6_list_prime[0]*mpo_uni10_br_2
- print trH2[0]
+ trH2=mpo_boundy_list[2]*mpo_uni10_6_list_prime[0]*mpo_uni10_br_2
+ #printtrH2[0]
  ###############################################################################
 
 
- #print mpo_uni10_6_list_prime[0].profile()
- #print mpo_uni10_6_list_prime[0].printDiagram()
+ ##printmpo_uni10_6_list_prime[0].profile()
+ ##printmpo_uni10_6_list_prime[0].printDiagram()
+###################  intialize_unitary_list #################################### 
+def  intialize_unitary_list( L, L_lay,d, delta): 
+ U_list=[]
+ bdi_spin = uni10.Bond(uni10.BD_IN, d);
+ bdo_spin = uni10.Bond(uni10.BD_OUT, d);
+ Svd=[]
+ matrix_random=uni10.Matrix(d*d, d*d)
+ for i in xrange(d*d):
+  for j in xrange(d*d):
+   if(i==j):
+    matrix_random[i*d*d+j]=1.0
+   else:
+    matrix_random[i*d*d+j]=0.0
+ #matrix_random.setIdentity()
+ for i in xrange(L):
+  U_list.append([]) 
+ for i in xrange(L):
+  for j in xrange(len(L_lay)):
+    U_uni10=uni10.UniTensor([bdi_spin, bdi_spin, bdo_spin,bdo_spin], "Unitary_uni10")
+    matrix_random1=copy.copy(matrix_random)
+    matrix_random1.randomize()
+    matrix_random1=matrix_random+(delta)*matrix_random1
+    Svd=matrix_random1.svd()
+    U_uni10.putBlock( Svd[0]*Svd[2] )
+    U_list[i].append(U_uni10)
+ return U_list 
+
+
+def unitary_list_transpose( U_list, L, L_lay):
+  U_list_Trans=[]
+  for i in xrange(L):
+   U_list_Trans.append([]) 
+  for i in xrange(L):
+   for j in xrange(len(L_lay)):
+    U_list_Trans[i].append(copy.copy(U_list[i][j]))
+    U_list_Trans[i][j].transpose()
+    U_list_Trans[i][j].setLabel([0,1,2,3])
+  return U_list_Trans
+    
+
+def  make_mpo_U(U_list, L_position, L_lay, L, Letter):
+ U=[copy.copy(U_list[L_position][i]) for i in xrange(len(L_lay))]
+ bdo = uni10.Bond(uni10.BD_OUT, 1)
+ bdi = uni10.Bond(uni10.BD_IN, 1)
+ Uni_out_1=uni10.UniTensor([bdo], "Uni_out_1")
+ Uni_in_1=uni10.UniTensor([bdo], "Uni_in_1")
+ Uni_in_1.identity()
+ Uni_out_1.identity()
+ if (L_position==0):
+  if (len(L_lay) == 1 ):
+    U0=copy.copy(U[0])
+    U0.combineBond([0,1])
+    U0.combineBond([2,3])
+    Uni_out_1.setLabel([1])
+    U_result=U0*Uni_out_1
+    U_result.permute([0,1,2],1)
+    ##printU_result.printDiagram()
+    if (Letter is 'up'):
+     return U_result
+    elif(Letter is 'down'):
+     U_result.permute([2,1,0],1)
+     U_result.setLabel([0,1,2])
+     return U_result
+  if (len(L_lay) == 2  ):
+    U0=copy.copy(U[0])
+    U1=copy.copy(U[1]) 
+    U0.setLabel([0,1,2,3])
+    U1.setLabel([3,4,5,6])
+    U_result=uni10.contract(U0,U1)
+    U_result.combineBond([0,1])
+    U_result.combineBond([2,5,6])
+    U_result.permute([0,4,2],1)
+    U_result.setLabel([0,1,2])
+    ##printU_result.printDiagram()
+    if (Letter is 'up'):
+     return U_result
+    elif(Letter is 'down'):
+     U_result.permute([2,1,0],1)
+     U_result.setLabel([0,1,2])
+     return U_result
+    
+  if (len(L_lay) == 3  ):
+    U0=copy.copy(U[0])
+    U1=copy.copy(U[1])
+    U2=copy.copy(U[2]) 
+    U0.setLabel([0,1,2,3])
+    U1.setLabel([3,4,5,6])
+    U2.setLabel([2,5,7,8])
+    U_result=((U0*U1)*U2)
+    U_result.combineBond([0,1])
+    U_result.combineBond([7,8])
+    U_result.combineBond([4,6])
+    U_result.permute([0,4,7],1)
+    U_result.setLabel([0,1,2])
+    #printU_result.#printDiagram()
+    if (Letter is 'up'):
+     return U_result
+    elif(Letter is 'down'):
+     U_result.permute([2,1,0],1)
+     U_result.setLabel([0,1,2])
+     return U_result
+    
+  if (len(L_lay) == 4  ):
+    U0=copy.copy(U[0])
+    U1=copy.copy(U[1])
+    U2=copy.copy(U[2])
+    U3=copy.copy(U[3]) 
+    U0.setLabel([0,1,2,3])
+    U1.setLabel([3,4,5,6])
+    U2.setLabel([2,5,7,8])
+    U3.setLabel([8,9,10,11])
+    U_result=((U0*U1)*(U2*U3))
+    U_result.combineBond([0,1])
+    U_result.combineBond([7,10,11])
+    U_result.combineBond([4,6,9])
+    U_result.permute([0,4,7],1)
+    U_result.setLabel([0,1,2])
+    #printU_result.printDiagram()
+    if (Letter is 'up'):
+     return U_result
+    elif(Letter is 'down'):
+     U_result.permute([2,1,0],1)
+     U_result.setLabel([0,1,2])
+     return U_result
+    
+  if (len(L_lay) == 5  ):
+    U0=copy.copy(U[0])
+    U1=copy.copy(U[1])
+    U2=copy.copy(U[2])
+    U3=copy.copy(U[3]) 
+    U4=copy.copy(U[4]) 
+    U0.setLabel([0,1,2,3])
+    U1.setLabel([3,4,5,6])
+    U2.setLabel([2,5,7,8])
+    U3.setLabel([8,9,10,11])
+    U4.setLabel([7,10,12,13])
+    U_result=(((U0*U1)*(U2*U3))*U4)
+    U_result.combineBond([0,1])
+    U_result.combineBond([12,13])
+    U_result.combineBond([4,6,9,11])
+    U_result.permute([0,4,12],1)
+    U_result.setLabel([0,1,2])
+    ##printU_result.printDiagram()
+    if (Letter is 'up'):
+     return U_result
+    elif(Letter is 'down'):
+     U_result.permute([2,1,0],1)
+     U_result.setLabel([0,1,2])
+     return U_result
+    
+ if (L_position==((L/2) - 1)):
+  if (len(L_lay) == 1  ):
+    U0=copy.copy(U[0])
+    U0.combineBond([0,1])
+    U0.combineBond([2,3])
+    Uni_out_1.setLabel([1])
+    U_result=U0*Uni_out_1
+    U_result.permute([0,1,2],2)
+    #printU_result.#printDiagram()
+    if (Letter is 'up'):
+     return U_result
+    elif(Letter is 'down'):
+     U_result.permute([2,1,0],2)
+     U_result.setLabel([0,1,2])
+     return U_result
+     
+  if (len(L_lay) == 2  ):
+      U0=copy.copy(U[0])
+      U0.combineBond([0,1])
+      U0.permute([0,2,3],2)
+      U0.setLabel([0,1,2])
+      U_result=copy.copy(U0)
+      ##printU0.printDiagram()
+      if (Letter is 'up'):
+       return U_result
+      elif(Letter is 'down'):
+       U_result.permute([2,1,0],2)
+       U_result.setLabel([0,1,2])
+       return U_result
+     
+  if (len(L_lay) == 3  ):
+      U0=copy.copy(U[0])
+      U0.combineBond([0,1])
+      U2=copy.copy(U[2])
+      U2.setLabel([4,3,5,6])
+      U2.combineBond([5,6])
+      U_result=U0*U2
+      U_result.combineBond([2,4])
+      U_result.permute([0,2,5],2)
+      U_result.setLabel([0,1,2])
+      ##printU_result.printDiagram()
+      if (Letter is 'up'):
+       return U_result
+      elif(Letter is 'down'):
+       U_result.permute([2,1,0],2)
+       U_result.setLabel([0,1,2])
+       return U_result
+  
+  if len(L_lay) is 4 :
+      U0=copy.copy(U[0])
+      U0.combineBond([0,1])
+      U2=copy.copy(U[2])
+      U2.setLabel([4,3,5,6])
+      U_result=U0*U2
+      U_result.combineBond([2,4,5])
+      U_result.permute([0,2,6],2)
+      U_result.setLabel([0,1,2])
+      #printU_result.printDiagram()
+      if (Letter is 'up'):
+       return U_result
+      elif(Letter is 'down'):
+       U_result.permute([2,1,0],2)
+       U_result.setLabel([0,1,2])
+       return U_result
+ 
+ 
+  if len(L_lay) is 5 :
+      U0=copy.copy(U[0])
+      U0.combineBond([0,1])
+      U2=copy.copy(U[2])
+      U2.setLabel([4,3,5,6])
+      U4=copy.copy(U[4])
+      U4.setLabel([7,6,8,9])
+      U4.combineBond([8,9])
+      U_result=((U0*U2)*(U4))
+      U_result.combineBond([2,4,5,7])
+      U_result.permute([0,2,8],2)
+      U_result.setLabel([0,1,2])
+      #printU_result.printDiagram()
+      if (Letter is 'up'):
+       return U_result
+      elif(Letter is 'down'):
+       U_result.permute([2,1,0],2)
+       U_result.setLabel([0,1,2])
+       return U_result
+      
+ if  (L_position is not ((L/2) - 1)) and (L_position is not 0)  : 
+  if len(L_lay) is 1  :
+    U0=copy.copy(U[0])
+    U0.combineBond([0,1])
+    U0.combineBond([2,3])
+    Uni_in_1.setLabel([5])
+    Uni_out_1.setLabel([4])
+    U_result=U0*Uni_in_1*Uni_out_1
+    U_result.permute([5,0,4, 2],2)
+    U_result.setLabel([0,1,2, 3])
+    #printU_result.printDiagram()
+    if (Letter is 'up'):
+     return U_result
+    elif(Letter is 'down'):
+     U_result.permute([0,3,2,1],2)
+     U_result.setLabel([0,1,2,3])
+     return U_result
+
+
+  if len(L_lay) is 2  :
+    U0=copy.copy(U[0])
+    U1=copy.copy(U[1]) 
+    U0.setLabel([0,1,2,3])
+    U1.setLabel([3,4,5,6])
+    U_result=uni10.contract(U0,U1)
+    U_result.combineBond([0,1])
+    U_result.combineBond([5,6])
+    U_result.permute([2,0,4,5],2)
+    U_result.setLabel([0,1,2,3])
+    #printU_result.printDiagram()
+    if (Letter is 'up'):
+     return U_result
+    elif(Letter is 'down'):
+     U_result.permute([0,3,2,1],2)
+     U_result.setLabel([0,1,2,3])
+     return U_result
+    
+  if (len(L_lay) == 3 ):
+    U0=copy.copy(U[0])
+    U1=copy.copy(U[1])
+    U2=copy.copy(U[2]) 
+    U0.setLabel([0,1,2,3])
+    U1.setLabel([3,4,5,6])
+    U2.setLabel([7,5,8,9])
+    U_result=((U0*U1)*U2)
+    U_result.combineBond([0,1])
+    U_result.combineBond([8,9])
+    U_result.combineBond([4,6])
+    U_result.combineBond([2,7])
+    U_result.permute([2,0,4,8],2)
+    U_result.setLabel([0,1,2,3])
+    #printU_result.printDiagram()
+    if (Letter is 'up'):
+     return U_result
+    elif(Letter is 'down'):
+     U_result.permute([0,3,2,1],2)
+     U_result.setLabel([0,1,2,3])
+     return U_result
+ 
+  if (len(L_lay) == 4 ):
+    U0=copy.copy(U[0])
+    U1=copy.copy(U[1])
+    U2=copy.copy(U[2])
+    U3=copy.copy(U[3]) 
+    U0.setLabel([0,1,2,3])
+    U1.setLabel([3,4,5,6])
+    U2.setLabel([7,5,8,9])
+    U3.setLabel([9,10,11,12])
+    U_result=((U0*U1)*(U2*U3))
+    U_result.combineBond([0,1])
+    U_result.combineBond([11,12])
+    U_result.combineBond([2,7,8])
+    U_result.combineBond([4,6,10])
+    U_result.permute([2,0,4,11],2)
+    U_result.setLabel([0,1,2,3])
+    #printU_result.printDiagram()
+    if (Letter is 'up'):
+     return U_result
+    elif(Letter is 'down'):
+     U_result.permute([0,3,2,1],2)
+     U_result.setLabel([0,1,2,3])
+     return U_result
+
+  if (len(L_lay) == 5  ):
+    U0=copy.copy(U[0])
+    U1=copy.copy(U[1])
+    U2=copy.copy(U[2])
+    U3=copy.copy(U[3]) 
+    U4=copy.copy(U[4]) 
+    U0.setLabel([0,1,2,3])
+    U1.setLabel([3,4,5,6])
+    U2.setLabel([7,5,8,9])
+    U3.setLabel([9,10,11,12])
+    U4.setLabel([13,11,14,15])
+    U_result=(((U0*U1)*(U2*U3))*U4)
+    U_result.combineBond([0,1])
+    U_result.combineBond([14,15])
+    U_result.combineBond([2,7,8,13])
+    U_result.combineBond([4,6,10,12])
+    U_result.permute([2,0,4,14],2)
+    U_result.setLabel([0,1,2,3])
+    #printU_result.printDiagram()
+    if (Letter is 'up'):
+     return U_result
+    elif(Letter is 'down'):
+     U_result.permute([0,3,2,1],2)
+     U_result.setLabel([0,1,2,3])
+     return U_result
+
+
+
+
+def make_mpo_U_list(U_list, L_lay, L, Letter):
+ mpo_U_list=[]
+ for i in xrange(L/2):
+  mpo_U_list.append(make_mpo_U(U_list,i, L_lay, L, Letter))
+ return mpo_U_list
+
+
+
 
--- var-TTN-MBL.py
+++ var-TTN-MBL.py
@@ -1,6 +1,6 @@
 import pyUni10 as uni10
-import sys
-import numpy as np
+#import sys
+#import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib
 import pylab
@@ -8,21 +8,66 @@ import random
 import copy
 import MPO
 
-L=8
-L_lay=[0,1,2]  #three layers
+L=14
+L_lay=[0,1,2,3,4]  #Numbers of layers <= 4
 d=2
 chi=5
-J=5
+W=5
+J=3.0
 hz_list=[]
-for i in xrange(L):
- hz_list.append(np.random.uniform(-J,J))
+U_delta=0.400  #if it's zero, U_list is intialize by Identity
 
-print hz_list
-hz_list_2=[x**2 for x in hz_list]
-print hz_list_2
-print sum(hz_list_2)
-trH2=(2.00**L)*(3.0/16.00*(L-1)+1.0/4.0*sum(hz_list_2))
-print  trH2
 
-mpo_list1, mpo_list2, mpo_bl1 , mpo_br1, mpo_bl2, mpo_br2=MPO.make_mpo_H(L, J, hz_list )
-MPO.contraction_MPO_trH2(mpo_list1, mpo_list2, mpo_bl1 , mpo_br1, mpo_bl2, mpo_br2 )
+
+MPO.intialize_coupling(L, hz_list, W)
+MPO.print_trH2(L, hz_list,J)
+
+
+
+
+# retrun MPO list, 
+mpo_list1, mpo_list2, mpo_boundy_list=MPO.make_mpo_H( L, J, hz_list )
+
+print len(mpo_list2)
+
+
+MPO.contraction_MPO_trH2( mpo_list1, mpo_list2, mpo_boundy_list )
+
+U_list=MPO.intialize_unitary_list( L, L_lay, d, U_delta)
+U_list_Trans=MPO.unitary_list_transpose( U_list, L, L_lay)
+
+
+
+
+#U1=copy.copy(U_list[4][2])
+#U1.setLabel([4,5,2,3])
+#print U1.transpose()
+#print U_list[4][2]*U1.transpose()
+ 
+L_position=6
+
+example_mpo_U=MPO.make_mpo_U(U_list,L_position, L_lay, L, 'up')
+
+mpo_U_list_up= MPO.make_mpo_U_list(U_list, L_lay, L, 'up')
+mpo_U_list_down= MPO.make_mpo_U_list(U_list, L_lay, L, 'down')
+
+
+example_mpo_Up=MPO.make_mpo_U(U_list,L_position, L_lay, L, 'down')
+
+L_lay_h=[0]
+for i in xrange(5):
+ mpo_U_list_up= MPO.make_mpo_U_list(U_list, L_lay_h, L, 'up')
+ mpo_U_list_down= MPO.make_mpo_U_list(U_list, L_lay_h, L, 'down')
+ L_lay_h.append([i+1])
+ mpo_U_list_down[5].setLabel([0,4,2,1])
+ #print mpo_U_list_down[0].printDiagram()
+ #mpo_U_list_down[5].setLabel([3,1,0])
+
+
+ print mpo_U_list_up[5]*mpo_U_list_down[5]
+
+
+
+
+
+
