85c6245093d24cb36c41c09265c2bd3af15b9720
sunchong137 <sunchong137@gmail.com>
2019-05-05 21:45:03 -0700

    qsampl

--- qsampl.py
+++ qsampl.py
@@ -42,7 +42,7 @@ def get_Hbond_tensor(J=1.0,Fieldz=1.0, bond_dim=2, edge=False,edgeside='L'):
     '''
     # make matrix
     sx   = matSx()
-    sy   = matSy()
+    #sy   = matSy()
     sz   = matSz()
     iden = matIden()
     Hbond_mat  = (-1.*J)*uni10.otimes(sx,sx)
@@ -65,77 +65,58 @@ def get_Hbond_tensor(J=1.0,Fieldz=1.0, bond_dim=2, edge=False,edgeside='L'):
     
     return Hbond
 
-'''
-#test1
-H1 = get_Hbond_tensor()
-H2 = get_Hbond_tensor(edge=True)
 
-print "H1:"
-print H1
-H2.setLabel([2,3,4,5])
-print "H2:"
-print H2
-print "+++++++"
-print H1*H2
-H3 = copy.copy(H2)
-H3.permute([4,5,2,3],2)
-H3.setLabel([2,3,4,5])
-print "H3"
-print H3
-print "=========="
-print H1*H3
-exit()
-'''         
+def get_unitary4bond_up(Ulist,L,bond_idx,bond_dim=2):
 
-
-def get_unitary4bond_up(Ulist,bond_idx,bond_dim=2,edge=False,edgeside='L'):
 
     # TODO Use bond_idx to detect edge bonds, how to detect right edge?
     # Ulist is a L/2 x 2 list, 2 is the number of layers 
+    # bond_idx ranges from 0 to L-2
+    NU1 = L/2
     l_layer = len(Ulist[0])
     if l_layer != 2:
         raise ValueError("Please make sure your unitary list has TWO layers!")
     
-    if edge:
-        if edgeside is 'L':
-            '''
-               _____
-          2 __|     |__ 5
-          3 __|  U  |__ 6
-          4 __|     |__ 7 
-              |_____| 
-        
-           Where 2,3 will combine with the Hamiltonian,
-           4 will combine with U_down
-            '''
-            U0 = copy.copy(Ulist[0][0])
-            U1 = copy.copy(Ulist[0][1])
-            U0.setLabel([0,1,2,3])
-            U1.setLabel([3,4,5,6])
-            U = U0*U1
-            U.setLabel([2,3,4,5,6,7])
+    if (bond_idx == 0):   # Left Edge
+        '''
+              _____
+         2 __|     |__ 5
+         3 __|  U  |__ 6
+         4 __|     |__ 7 
+             |_____| 
+    
+        Where 2,3 will combine with the Hamiltonian,
+        4 will combine with U_down
+        '''
+        U0 = copy.copy(Ulist[0][0])
+        U1 = copy.copy(Ulist[0][1])
+        U0.setLabel([0,1,2,3])
+        U1.setLabel([3,4,5,6])
+        U = U0*U1
+        U.permute([0,1,4,2,5,6],3)
+        U.setLabel([2,3,4,5,6,7])
 
-        elif edgeside is 'R':
-            '''
-               _____
-          4 __|     |__ 5
-          2 __|  U  |__ 6
-          3 __|     |__ 7 
-              |_____| 
-        
-           Where 2,3 will combine with the Hamiltonian,
-           4 will combine with U_down
-            '''
+    elif (bond_idx == L-2):   # Right Edge
+        '''
+              _____
+         4 __|     |__ 5
+         2 __|  U  |__ 6
+         3 __|     |__ 7 
+             |_____| 
+       
+        Where 2,3 will combine with the Hamiltonian,
+        4 will combine with U_down
+        '''
 
-            U0 = copy.copy(Ulist[-1][0])
-            U1 = copy.copy(Ulist[-1][1])
-            U0.setLabel([0,1,2,3])
-            U1.setLabel([4,2,5,6])
-            U = U0*U1
-            U.setLabel([4,2,3,5,6,7])
-        else:
-            raise ValueError("The edgeside argument can only be 'L' or 'R'! ")
+        U0 = copy.copy(Ulist[NU1-1][0])
+        U1 = copy.copy(Ulist[NU1-2][1])
+        U0.setLabel([0,1,2,3])
+        U1.setLabel([4,2,5,6])
+        U = U0*U1
+        U.permute([4,0,1,5,6,3],3)
+        U.setLabel([4,2,3,5,6,7])
     else:
+
 
         '''
             _____
@@ -149,71 +130,115 @@ def get_unitary4bond_up(Ulist,bond_idx,bond_dim=2,edge=False,edgeside='L'):
         4,-4 will combine with U_down
         '''
 
-        if(bond_idx//2==0):
+        if(bond_idx%2 == 0):   # Even Bond
             l = bond_idx/2
             U0  = copy.copy(Ulist[l][0])
             U1L = copy.copy(Ulist[l-1][1])
             U1R = copy.copy(Ulist[l][1])
             U0.setLabel([0,1,2,3])
-            U1L.setLabel([3,4,5,6])
-            U1R.setLabel([7,2,8,9])
+            U1L.setLabel([4,2,5,6])
+            U1R.setLabel([3,7,8,9])
             U = (U0*U1L)*U1R
+            U.permute([4,0,1,7,5,6,8,9],4)
             U.setLabel([4,2,3,-4,5,6,7,8]) 
 
-        else:
+        else:                # Odd Bond
             l = bond_idx//2
             U0L = copy.copy(Ulist[l][0])
             U0R = copy.copy(Ulist[l+1][0])
             U1  = copy.copy(Ulist[l][1])
             U0L.setLabel([0,1,2,3])
             U0R.setLabel([4,5,6,7])
-            U1.setLabel([7,2,8,9])
+            U1.setLabel([3,6,8,9])
             U = U0L*(U0R*U1)
+            U.permute([0,1,4,5,2,8,9,7],4)
             U.setLabel([4,2,3,-4,5,6,7,8]) 
-    return U
 
-'''
-#test2
-U = get_unitary4bond_up(U_list,0,edge=True) 
-print U
-'''
+    return U
 
 def contract_UHU(L, Ulist):
     '''
     Return a list of U^+HU.
     L is the lattice length.
     '''
+    #TODO Consider when U has imaginary parts
 
     UHU = []
     # left edge
     Hbond = get_Hbond_tensor(edge=True,edgeside='L') 
-    U_up  = get_unitary4bond_up(Ulist,0,edge=True,edgeside='L')
+    U_up  = get_unitary4bond_up(Ulist,L,0)
     U_dn  = copy.copy(U_up)
     U_dn.permute([5,6,7,2,3,4],3)
     U_dn.setLabel([8,9,10,0,1,4])
     Hbond_n = (U_dn*Hbond)*U_up
+    Hbond_n.setLabel([0,1,2,3,4,5])
     UHU.append(Hbond_n)
 
     for bidx in range(1,L-2):
         Hbond = get_Hbond_tensor(edge=False)
-        U_up  = get_unitary4bond_up(Ulist,bidx)
+        U_up  = get_unitary4bond_up(Ulist,L,bidx)
         U_dn  = copy.copy(U_up)
         U_dn.permute([5,6,7,8,4,2,3,-4],4)
         U_dn.setLabel([9,10,11,12,4,0,1,-4])
         Hbond_n = (U_dn*Hbond)*U_up
+        Hbond_n.setLabel([0,1,2,3,4,5,6,7])
         UHU.append(Hbond_n)
 
     Hbond = get_Hbond_tensor(edge=True,edgeside='R')
-    U_up  = get_unitary4bond_up(Ulist,L-1,edge=True,edgeside='R')
+    U_up  = get_unitary4bond_up(Ulist,L,L-2)
     U_dn  = copy.copy(U_up)
     U_dn.permute([5,6,7,4,2,3],3)
     U_dn.setLabel([8,9,10,4,0,1])
     Hbond_n = (U_dn*Hbond)*U_up
+    Hbond_n.setLabel([0,1,2,3,4,5])
     UHU.append(Hbond_n)
 
     return UHU
+
+######PAULI######PAULI######PAULI######PAULI######PAULI######PAULI
+def pauli2body():
+    # TODO figure out an easier way to print the labels
+    bond_dim = 2
+    iden = matIden()
+    sx   = matSx()
+    sy   = matSy()
+    sz   = matSz()
+    bdi = uni10.Bond(uni10.BD_IN,  bond_dim)
+    bdo = uni10.Bond(uni10.BD_OUT, bond_dim)
+
+    pauli1b = [iden,sx,sy,sz]
+    pauli2b = []
+    pauli2b_labels = [['II','IX','IY','IZ'],\
+                      ['XI','XX','XY','XZ'],\
+                      ['YI','YX','YY','YZ'],\
+                      ['ZI','ZX','ZY','ZZ']]            
+    for i in range (4):
+        for j in range(4):
+            mat = uni10.otimes(pauli1b[i],pauli1b[j])
+            P   = uni10.UniTensor([bdi,bdi,bdo,bdo],pauli2b_labels[i][j])
+            P.putBlock(mat)
+            pauli2b.append(P) 
+
+    return pauli2b, pauli2b_labels
+            
+    
+def unitary2pauli(U,P,lp):
+    '''
+    U  - unitary
+    P  - Pauli string
+    lp - length of Pauli string 
+    '''
+    U_ = copy.copy(U)
+    U_.setLabel([0,1,2,3])
+    trUP = (U_*P).norm() / (2.*lp)
+    return trUP
+
+
     
     
+    
+    
+
             
         
 
---
+++ tests/debug/u2p_debug.py
@@ -0,0 +1,60 @@
+import numpy as np
+import sys
+sys.path.append("/Users/sunchong/CodingDir/TNMBL")
+from qsampl import *
+import pyUni10 as uni10
+from scipy import linalg as sla
+import copy 
+
+
+L=6
+
+# ### bond construction
+sx = np.array([0.,1.,1.,0.]).reshape(2,2)
+sy = np.array([0.,-1.j,1.j,0.]).reshape(2,2)
+sz = np.array([1.,0.,0.,-1.]).reshape(2,2)
+si = np.eye(2)
+Sx = uni10.Matrix(2,2,[0.,1.,1.,0.])
+Sy = uni10.Matrix(2,2,[0.,-1.j,1.j,0.])
+Sz = uni10.Matrix(2,2,[1.,0.,0.,-1.])
+Si = uni10.Matrix(2,2,[1.,0.,0.,1.])
+
+bdi = uni10.Bond(uni10.BD_IN,2)
+bdo = uni10.Bond(uni10.BD_OUT,2)
+
+u11 = np.einsum('ij,kl->ikjl',sx,sz)
+u12 = np.einsum('ij,kl->ikjl',si,sx)
+u13 = np.einsum('ij,kl->ikjl',sz,sz)
+u21 = np.einsum('ij,kl->ikjl',sz,sx)
+u22 = np.einsum('ij,kl->ikjl',sz,si)
+u23 = np.einsum('ij,kl->ikjl',sx,sx)
+
+
+U11 = uni10.UniTensor([bdi,bdi,bdo,bdo])
+U11.putBlock(uni10.otimes(Sx,Sz))
+U12 = uni10.UniTensor([bdi,bdi,bdo,bdo])
+U12.putBlock(uni10.otimes(Si,Sx))
+U13 = uni10.UniTensor([bdi,bdi,bdo,bdo])
+U13.putBlock(uni10.otimes(Sz,Sz))
+U21 = uni10.UniTensor([bdi,bdi,bdo,bdo])
+U21.putBlock(uni10.otimes(Sz,Sx))
+U22 = uni10.UniTensor([bdi,bdi,bdo,bdo])
+U22.putBlock(uni10.otimes(Sz,Si))
+U23 = uni10.UniTensor([bdi,bdi,bdo,bdo])
+U23.putBlock(uni10.otimes(Sx,Sx))
+
+def test_u2p():
+    umat = uni10.Matrix(4,4,[0. ,  0. ,  0.9,  0.,0. ,  0. ,  0. ,  0.1,0.9,  0. ,  0. ,  0.,0. ,  0.1,  0. ,  0.])
+    U = uni10.UniTensor([bdi,bdi,bdo,bdo],'U')
+    U.putBlock(umat)
+    pstr, _ = pauli2body()
+    coef = []
+    for i in range(len(pstr)):
+        c = unitary2pauli(U,pstr[i],2)
+        coef.append(c)
+
+    print coef
+
+test_u2p()
+    
+ 
